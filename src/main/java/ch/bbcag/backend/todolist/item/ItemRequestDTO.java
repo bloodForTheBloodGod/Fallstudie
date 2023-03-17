8a63eb36d8bc78a602bff4de53e0eef2f7b320ee
Julian Kläsi <bklaej@bbcag.ch>
2023-03-17 11:26:47 +0100

    DTOs für Item gemacht.

--- src/main/java/ch/bbcag/backend/todolist/item/ItemRequestDTO.java
+++ src/main/java/ch/bbcag/backend/todolist/item/ItemRequestDTO.java
@@ -1,4 +1,56 @@
 package ch.bbcag.backend.todolist.item;
 
+import java.sql.Timestamp;
+
 public class ItemRequestDTO {
+
+    public String description;
+
+    private Timestamp deletedAt;
+
+    private Integer personId;
+
+    private String name;
+
+    private Timestamp doneAt;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Timestamp deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+    public Integer getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Integer personId) {
+        this.personId = personId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Timestamp getDoneAt() {
+        return doneAt;
+    }
+
+    public void setDoneAt(Timestamp doneAt) {
+        this.doneAt = doneAt;
+    }
 }
--- src/main/java/ch/bbcag/backend/todolist/item/ItemResponseDTO.java
+++ src/main/java/ch/bbcag/backend/todolist/item/ItemResponseDTO.java
@@ -1,4 +1,26 @@
 package ch.bbcag.backend.todolist.item;
 
-public class ItemResponseDTO {
+import java.sql.Timestamp;
+
+public class ItemResponseDTO extends ItemRequestDTO{
+
+    public Integer id;
+
+    private Timestamp createdAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
 }
